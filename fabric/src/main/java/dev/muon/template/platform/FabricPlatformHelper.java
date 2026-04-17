e9e31a83c829109ed6fa4a0625f80bd0e4dace35
muon-rw <pashah012@gmail.com>
2026-04-17 00:06:59 -0700

    Rename, add deps, fix runs

---
+++ fabric/src/main/java/dev/muon/template/platform/FabricPlatformHelper.java
@@ -0,0 +1,24 @@
+package dev.muon.template.platform;
+
+import dev.muon.template.platform.services.IPlatformHelper;
+import net.fabricmc.loader.api.FabricLoader;
+
+public class FabricPlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+        return "Fabric";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return FabricLoader.getInstance().isModLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return FabricLoader.getInstance().isDevelopmentEnvironment();
+    }
+}
